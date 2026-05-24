089d1322e8f6f5a4925cb14cbb9bd7d9474e5a60
AngelGonzalezJEJE <angel.liberto.ceb@immune.institute>
2026-05-24 14:18:34 +0200

    fix: remove metro blockList that broke keyboard-controller bundle

--- metro.config.js
+++ metro.config.js
@@ -4,9 +4,4 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-// Ignorar carpetas que quieras excluir
-config.resolver.blockList = [
-  /.*[\/]types[\/].*/, 
-];
-
 module.exports = withNativeWind(config, { input: './global.css' });
